f2e431d9a05f4f4a6da656bc79c56548c0285210
Harris Sultan <harris_sultan@outlook.com>
2022-02-06 19:33:01 -0500

    commit tree f8facd291fd1d04b1787d8f032799277274ee440

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,20 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] = useState("#ffffff");
+
+
+
+function handleChangeColor(childColor) {
+  const newRandomColor = getRandomColor();
+  setChildColor(childColor);
+  setColor(newRandomColor);
+}
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childColor} onChangeColor={handleChangeColor}/>
+      <Child color={childColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
